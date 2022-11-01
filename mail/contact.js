766c583b39bed92d58facdd4909c9a5906d4a0e4
Irshad Khaleel P <97274646+IrshadSTX@users.noreply.github.com>
2022-11-01 13:01:10 +0530

    Add files via upload

---
+++ mail/contact.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+        },
+        submitSuccess: function ($form, event) {
+            // event.preventDefault();
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var subject = $("input#subject").val();
+            var message = $("textarea#message").val();
+
+            // $this = $("#sendMessageButton");
+            // $this.prop("disabled", true);
+
+        
+             
+                
+                
+            
+        },
+       
+    });
+
+  
+});
+
+
